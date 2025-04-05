59307b60cc6be0c5fe5dab778accd6410c5bc316
Roland Fuhrer <roland@resterland.ch>
2025-04-05 13:21:15 +0200

    Initial configuration

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'rlt3-fluid-package',
+    'description' => 'ResterLand TYPO3 fluid site package',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+            'fluid_styled_content' => '13.4.0-13.4.99',
+            'rte_ckeditor' => '13.4.0-13.4.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Resterland\\Rlt3FluidPackage\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Roland Fuhrer',
+    'author_email' => 'webmaster@resterland.org',
+    'author_company' => 'ResterLand',
+    'version' => '1.0.0',
+];
---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('TYPO3') or die('Access denied.');
+
+// Add default RTE configuration
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['rlt3_fluid_package'] = 'EXT:rlt3_fluid_package/Configuration/RTE/Default.yaml';
